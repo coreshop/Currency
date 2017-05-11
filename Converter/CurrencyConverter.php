f537dd0c8d4c6cede652a27b8096e142cafa9156
dpfaffenbauer <dominik@lineofcode.at>
2017-05-11 18:25:31 +0200

    [Refactor] move all to src/CoreShop folder

---
+++ Converter/CurrencyConverter.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Component\Currency\Converter;
+
+use CoreShop\Component\Currency\Model\CurrencyInterface;
+use CoreShop\Component\Currency\Repository\CurrencyRepositoryInterface;
+
+final class CurrencyConverter implements CurrencyConverterInterface
+{
+    /**
+     * @var CurrencyRepositoryInterface
+     */
+    private $currencyRepository;
+
+    /**
+     * @param CurrencyRepositoryInterface $currencyRepository
+     */
+    public function __construct(CurrencyRepositoryInterface $currencyRepository)
+    {
+        $this->currencyRepository = $currencyRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convert($amount, $sourceCurrencyCode, $targetCurrencyCode)
+    {
+        if ($sourceCurrencyCode === $targetCurrencyCode) {
+            return $amount;
+        }
+
+        /**
+         * @var CurrencyInterface
+         */
+        $sourceCurrency = $this->currencyRepository->getByCode($sourceCurrencyCode);
+        /**
+         * @var CurrencyInterface
+         */
+        $targetCurrency = $this->currencyRepository->getByCode($targetCurrencyCode);
+
+        return $amount * $targetCurrency->getExchangeRate();
+    }
+}
